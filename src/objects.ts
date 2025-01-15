ab3f2a7ebbdbc4ebbdd774f749c6503139731c98
Alejandra Arteaga Avelar <106767682+alejandragaavelar@users.noreply.github.com>
2025-01-15 10:51:23 -0700

    cf2
    cf2\

---
+++ src/objects.ts
@@ -0,0 +1,14 @@
+type Person = {
+    firstName?: string;
+    lastName?: string;
+    age?: number;
+};
+
+
+const rufus: Person = {
+    firstName: "Rufus",
+    lastName: "McQueue",
+    age: 65,
+};
+
+console.log(rufus);
---
+++ src/variable2.ts
@@ -0,0 +1,8 @@
+let myName = "Alejandra";
+myName = "Rufus";
+const myAge = 21;
+const isHungry = true;
+
+
+console.log("My name is:", myName, "and I am", myAge, "years old");
+
